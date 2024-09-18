8dfb165c42ebc2fe8cdf417d00afaff444373a18
Jack Sari <janasarii@gmail.com>
2024-09-18 16:40:12 -0500

    add

--- app/Traits/PdfTrait.php
+++ app/Traits/PdfTrait.php
@@ -51,6 +51,12 @@ trait PdfTrait
                 }
             }
         }
+        //filtrar unicos
+        $coberturas = array_values($coberturas->unique('concepto_id')->all());
+        //eliminar los que tienen concepto_id null
+        $coberturas = array_filter($coberturas, function ($item) {
+            return $item['concepto_id'] != null;
+        });
         $data->coberturas = $coberturas;
 
          //extract deducibles
